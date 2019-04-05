11cbaef6731d1a6e413c41bf4af9149e8d5f4f93
Jan Hoffmann <j.hoffmann@appcom-interactive.de>
2019-04-05 09:47:31 +0200

    refactor(test): use chai for expectations

--- test/src/zipreader.test.js
+++ test/src/zipreader.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+const expect = require('chai').expect;
 const { describe, it } = require("mocha");
 const ZipFileReadStream = require('../../index');
 
@@ -8,58 +8,58 @@ describe('ZipReadStream', () => {
     describe('Reading', () => {
         it('#single uncompressed file', async () => {
             const files = await zipReader('./test/resources/single-file.zip');
-            assert.equal(files.length, 1);
-            assert.equal(files[0].name, 'test.txt');
-            assert.equal(files[0].method, 'STORE');
-            assert.equal(files[0].content, `Hallo\n`);
+            expect(files.length).to.equal(1);
+            expect(files[0].name).to.equal('test.txt');
+            expect(files[0].method).to.equal('STORE');
+            expect(files[0].content).to.equal(`Hallo\n`);
         });
 
         it('#multiple compressed files', async () => {
             const files = await zipReader('./test/resources/multiple-files.zip');
-            assert.equal(files.length, 3);
-            assert.equal(files[0].name, 'lorem_1.txt');
-            assert.equal(files[0].method, 'DEFLATE');
-            assert.equal(files[0].content.startsWith('Lorem ipsum'), true);
-            assert.equal(files[1].name, 'lorem_2.txt');
-            assert.equal(files[1].method, 'DEFLATE');
-            assert.equal(files[1].content.startsWith('Lorem ipsum'), true);
-            assert.equal(files[2].name, 'lorem_3.txt');
-            assert.equal(files[2].method, 'DEFLATE');
-            assert.equal(files[2].content.startsWith('Lorem ipsum'), true);
+            expect(files.length).to.equal(3);
+            expect(files[0].name).to.equal('lorem_1.txt');
+            expect(files[0].method).to.equal('DEFLATE');
+            expect(files[0].content).to.match(/^Lorem ipsum/);
+            expect(files[1].name).to.equal('lorem_2.txt');
+            expect(files[1].method).to.equal('DEFLATE');
+            expect(files[1].content).to.match(/^Lorem ipsum/);
+            expect(files[2].name).to.equal('lorem_3.txt');
+            expect(files[2].method).to.equal('DEFLATE');
+            expect(files[2].content).to.match(/^Lorem ipsum/);
         });
 
         it('#zip64 support', done => {
             const zipStream = new ZipFileReadStream('./test/resources/zip64_stored.zip');
-            assert.equal(zipStream.fileCount, 65537);
-            let file = null;
+            expect(zipStream.fileCount).to.equal(65537);
+            let zipFile;
             zipStream.on('data', file => {
-                file = {
+                zipFile = {
                     name: file.metaInfo.fileName,
                     method: file.metaInfo.cMethodName,
                     content: file.content.toString()
                 };
                 zipStream.pause();
-                assert.equal(file.name, '0.txt');
-                assert.equal(file.method, 'STORE');
-                assert.equal(file.content, 'Lorem Ipsum');
+                expect(zipFile.name).to.equal('0.txt');
+                expect(zipFile.method).to.equal('STORE');
+                expect(zipFile.content).to.equal('Lorem Ipsum');
                 done();
             });
         });
 
         it('#zip64 deflate support', done => {
             const zipStream = new ZipFileReadStream('./test/resources/zip64_deflate.zip');
-            assert.equal(zipStream.fileCount, 65537);
-            let file = null;
+            expect(zipStream.fileCount).to.equal(65537);
+            let zipFile;
             zipStream.on('data', file => {
-                file = {
+                zipFile = {
                     name: file.metaInfo.fileName,
                     method: file.metaInfo.cMethodName,
                     content: file.content.toString()
                 };
                 zipStream.pause();
-                assert.equal(file.name, '0.txt');
-                assert.equal(file.method, 'DEFLATE');
-                assert.equal(file.content.startsWith('Lorem ipsum'), true);
+                expect(zipFile.name).to.equal('0.txt');
+                expect(zipFile.method).to.equal('DEFLATE');
+                expect(zipFile.content).to.match(/^Lorem ipsum/);
                 done();
             });
         });
@@ -70,7 +70,7 @@ describe('ZipReadStream', () => {
             try {
                 await zipReader('./test/resources/unknown-compression-method.zip');
             } catch (e) {
-                assert.equal(e.message, `Unsupported compression method for file 'lorem_1.txt'`);
+                expect(e.message).to.equal(`Unsupported compression method for file 'lorem_1.txt'`);
             }
         });
 
@@ -78,7 +78,7 @@ describe('ZipReadStream', () => {
             try {
                 await zipReader('./test/resources/not-existing.zip');
             } catch (e) {
-                assert.equal(e.message, './test/resources/not-existing.zip does not exist.');
+                expect(e.message).to.equal('./test/resources/not-existing.zip does not exist.');
             }
         });
 
@@ -86,7 +86,7 @@ describe('ZipReadStream', () => {
             try {
                 await zipReader('./test/resources/not-a-zip-file.zip');
             } catch (e) {
-                assert.equal(e.message, './test/resources/not-a-zip-file.zip is not a valid zip file or empty.');
+                expect(e.message).to.equal('./test/resources/not-a-zip-file.zip is not a valid zip file or empty.');
             }
         });
     });
